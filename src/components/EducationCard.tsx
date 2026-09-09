c9082e192194746c03a0fcb4e57e3829e1c9fd37
Renz Frederick <d3nzus@gmail.com>
2026-09-09 18:56:30 +0800

    refactor: styling on ui components

--- src/components/EducationCard.tsx
+++ src/components/EducationCard.tsx
@@ -2,6 +2,7 @@ function EducationCard({ degree, school, duration }: { degree: string; school: s
     return (
         <div className="bg-gray-500 rounded-4xl w-3/4 h-fit mx-auto p-5 my-2 flex flex-col align-baseline justify-start">
             <p className="text-white text-xl">{degree}</p>
+            <hr className="my-2 border-0 border-t border-gray-300/50" />
             <p className="text-white text-md">{school}</p>
             <p className="text-white text-sm">{duration}</p>
         </div>
